aba9827e705a08273d7d9310f2d6bf851dd2fb9b
karlderkaefer <karlderkaefer@users.noreply.github.com>
2021-09-01 22:00:12 +0200

    fix: parsing of CIRCLE_PULL_REQUEST

--- config/app.go
+++ config/app.go
@@ -1,10 +1,11 @@
 package config
 
 import (
-	"errors"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type AppConfig struct {
@@ -17,31 +18,51 @@ type AppConfig struct {
 	DeleteComment bool
 }
 
+type ConfigValidationError struct {
+	cliArg string
+	envVar string
+}
+
+func (e *ConfigValidationError) Error() string {
+	return fmt.Sprintf("missing argument. Set --%s argument or env var %s", e.cliArg, e.envVar)
+}
+
+const (
+	ENV_GITHUB_TOKEN    = "GITHUB_TOKEN"
+	ENV_PULL_REQUEST_ID = "CIRCLE_PULL_REQUEST"
+	ENV_REPO_NAME       = "CIRCLE_PROJECT_REPONAME"
+	ENV_REPO_OWNER      = "CIRCLE_PROJECT_USERNAME"
+)
+
 func (a *AppConfig) Init() error {
 	if a.RepoName == "" {
-		a.RepoName = readFromEnv("CIRCLE_PROJECT_REPONAME")
+		a.RepoName = readFromEnv(ENV_REPO_NAME)
 	}
 	if a.RepoOwner == "" {
-		a.RepoOwner = readFromEnv("CIRCLE_PROJECT_USERNAME")
+		a.RepoOwner = readFromEnv(ENV_REPO_OWNER)
 	}
 	if a.GithubToken == "" {
-		a.GithubToken = readFromEnv("GITHUB_TOKEN")
+		a.GithubToken = readFromEnv(ENV_GITHUB_TOKEN)
 	}
 	if a.PullRequest == 0 {
-		a.PullRequest = readIntFromEnv("CIRCLE_PR_NUMBER")
+		prNumber, err := readPullRequestFromEnv()
+		if err != nil {
+			return err
+		}
+		a.PullRequest = prNumber
 	}
 	// validate
 	if a.RepoName == "" {
-		return errors.New("missing argument. Set github-repo argument or env var CIRCLE_PROJECT_REPONAME")
+		return &ConfigValidationError{"github-repo", ENV_REPO_NAME}
 	}
 	if a.RepoOwner == "" {
-		return errors.New("missing argument. Set github-owner argument or env var CIRCLE_PROJECT_USERNAME")
+		return &ConfigValidationError{"github-owner", ENV_REPO_OWNER}
 	}
 	if a.GithubToken == "" {
-		return errors.New("missing argument. Set github-token argument or env var GITHUB_TOKEN")
+		return &ConfigValidationError{"github-token", ENV_GITHUB_TOKEN}
 	}
 	if a.PullRequest == 0 {
-		return errors.New("missing argument. Set pull-request-id argument or env var CIRCLE_PR_NUMBER")
+		return &ConfigValidationError{"pull-request-id", ENV_PULL_REQUEST_ID}
 	}
 	return nil
 }
@@ -49,18 +70,23 @@ func (a *AppConfig) Init() error {
 func readFromEnv(env string) string {
 	val := os.Getenv(env)
 	if val != "" {
-		logrus.Debugf("Reading env var %s with value %s", env, val)
+		logrus.Debugf("Reading env var %s with value '%s'", env, val)
 	}
 	return val
 }
 
-func readIntFromEnv(env string) int {
-	val, err := strconv.ParseInt(os.Getenv(env), 10, 0)
+// CIRCLE_PULL_REQUEST=https://github.com/signavio/suite-infrastructure/pull/1361
+func readPullRequestFromEnv() (int, error) {
+	url := os.Getenv(ENV_PULL_REQUEST_ID)
+	elements := strings.Split(url, "/")
+	prNumber := elements[len(elements)-1]
+	val, err := strconv.ParseInt(prNumber, 10, 0)
 	if err != nil {
-		logrus.Fatalf("Can not parse int from env var %s", env)
+		logrus.Errorf("Could not parse env %s with value '%v' to int", ENV_PULL_REQUEST_ID, url)
+		return 0, err
 	}
 	if val != 0 {
-		logrus.Debugf("Reading env var %s with value %d", env, val)
+		logrus.Debugf("Reading env var %s with value '%d'", ENV_PULL_REQUEST_ID, val)
 	}
-	return int(val)
+	return int(val), nil
 }
